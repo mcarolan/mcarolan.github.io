72898aa7df0a46a14cf4e61091ed443e6f939ceb
Martin Carolan <mail@mcarolan.net>
2020-08-30 12:15:46 +0100

    Add numero

---
+++ numero/precache-manifest.e5b7b9ea5de49acec335ee6025fc80cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f364198ddc82cd2bc47d1d3e6a9f61a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b064d1ebf3c38e37a086",
+    "url": "/static/css/main.4dabebdd.chunk.css"
+  },
+  {
+    "revision": "2f868cc77ff28ff4693d",
+    "url": "/static/js/2.318973d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.318973d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b064d1ebf3c38e37a086",
+    "url": "/static/js/main.9ddac265.chunk.js"
+  },
+  {
+    "revision": "e5ac80f2a2fddc4281d7",
+    "url": "/static/js/runtime-main.9ced04be.js"
+  }
+]);
